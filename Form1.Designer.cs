c42fa04767b3ae375a96d59e9c2d0961d6895231
mikeErmantrautKuznetsov <128633238+mikeErmantrautKuznetsov@users.noreply.github.com>
2024-07-27 00:24:12 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace Launcher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DarkOrchid;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.Color.LightSkyBlue;
+            this.button1.Location = new System.Drawing.Point(309, 61);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(141, 51);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Play";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(110, 344);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(530, 23);
+            this.progressBar1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.DarkOrchid;
+            this.label1.ForeColor = System.Drawing.Color.SkyBlue;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "LauncherGame";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,155 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Net;
+using System.Net.Cache;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Launcher
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            ServicePointManager.Expect100Continue = true;
+            ServicePointManager.ServerCertificateValidationCallback += (sender, certificate, chain, sslPolicyErrors) => true;
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12 | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls;
+        }
+
+        public string version = "";
+        public string serverVersion = "";
+        public string path = @"Game\";
+        public string exeName = "TestWork2Games.exe";
+        public string url = "https://launcher-game.local/";
+        public bool downLoading = false;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Directory.CreateDirectory(Path.GetFullPath(path));
+            if (!File.Exists(Path.GetFullPath(path + exeName)))
+            {
+                RegistryKey key;
+                key = Registry.CurrentUser.CreateSubKey("GamesWork");
+                key.SetValue("ver", "none");
+                key.Close();
+                version = "none";
+            }
+            else
+            {
+                version = (string)Registry.CurrentUser.OpenSubKey("GamesWork").GetValue("ver");
+            }
+
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url + "index.php");
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                Stream recivestream = response.GetResponseStream();
+                StreamReader readSteam = null;
+                if (response.CharacterSet == null)
+                {
+                    readSteam = new StreamReader(recivestream);
+                }
+                else
+                {
+                    readSteam = new StreamReader(recivestream, Encoding.GetEncoding(response.CharacterSet));
+                }
+
+                string date = readSteam.ReadToEnd();
+                serverVersion = date;
+                response.Close();
+                readSteam.Close();
+            }
+
+            else
+            {
+                MessageBox.Show("404");
+            }
+
+            label1.Text = version + "/" + serverVersion;
+
+            if (version != serverVersion)
+            {
+                if (File.Exists(Path.GetFullPath(path + exeName)))
+                {
+                    button1.Text = "Update";
+                    button1.Click += UpdateGame;
+                }
+                else
+                {
+                    button1.Text = "DownGame";
+                    button1.Click += UpdateGame;
+                }
+            }
+            else
+            {
+                button1.Text = "Start";
+                button1.Click += Play;
+            }
+
+
+        }
+
+        private void UpdateGame(object sender, EventArgs e)
+        {
+            downLoading = false;
+            button1.Enabled = false;
+            DirectoryInfo dir = new DirectoryInfo(Path.GetFullPath(path));
+            foreach (FileInfo file in dir.GetFiles())
+            {
+                file.Delete();
+            }
+            foreach (DirectoryInfo file in dir.GetDirectories())
+            {
+                file.Delete(true);
+            }
+
+            using (WebClient webClient = new WebClient())
+            {
+                webClient.DownloadProgressChanged += (s, g) =>
+                {
+                    if (g.ProgressPercentage == 100)
+                    {
+                        if (!File.Exists(Path.GetFullPath(path + exeName)))
+                        {
+                            ZipFile.ExtractToDirectory("game.zip", path);
+                            RegistryKey key;
+                            key = Registry.CurrentUser.CreateSubKey("GamesWork");
+                            key.SetValue("ver", serverVersion);
+                            key.Close();
+                            version = serverVersion;
+                            label1.Text = version + "/" + serverVersion;
+
+                            button1.Text = "Play";
+                            button1.Click -= UpdateGame;
+                            button1.Click += Play;
+
+                        }
+
+                        button1.Enabled = true;
+                        downLoading = false;
+                    }
+                };
+
+                webClient.DownloadFileAsync(new Uri(url + "TestWork2Games.zip"), "Game.zip");
+            }
+        }
+
+        private void Play(object sender, EventArgs e)
+        {
+            Process.Start(path + exeName);
+        }
+    }
+}
